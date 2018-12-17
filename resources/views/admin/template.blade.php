b95efbe553ef6f66ae3cf606553392fc3caac224
Salvador Montiel <salvadormontiel8@gmail.com>
2018-12-17 11:57:39 -0600

    Add download apk link

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -60,6 +60,9 @@
                                     <a class="dropdown-item" href="/admin">
                                         {{ __('Admin') }}
                                     </a>
+                                    <a class="dropdown-item" href="/apk/Ferreteria-Admin.apk">
+                                        Descargar apk
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
